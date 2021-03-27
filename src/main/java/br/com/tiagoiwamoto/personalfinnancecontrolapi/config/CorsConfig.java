ae1825a5732e22d70a451ff84963d0505eefc46e
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2021-03-27 07:49:09 -0300

    Adicionando configuração de cors

---
+++ src/main/java/br/com/tiagoiwamoto/personalfinnancecontrolapi/config/CorsConfig.java
@@ -0,0 +1,39 @@
+package br.com.tiagoiwamoto.personalfinnancecontrolapi.config;
+
+/*
+ * Tiago Henrique Iwamoto
+ * tiago.iwamoto@gmail.com
+ * linkedin.com/in/tiago-iwamoto
+ * System specialist
+ * 27/03/2021 | 07:46
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Slf4j
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        log.info("starting the configuration of cors");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
